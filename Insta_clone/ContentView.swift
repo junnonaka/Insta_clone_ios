a4a6ed1e95b28b87eb954523d4dbf3ce71d99e56
junnonaka <your_email@example.com>
2025-09-21 12:10:38 +0900

    Initial Commit

---
+++ Insta_clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Insta_clone
+//
+//  Created by 野中淳 on 2025/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Insta_clone/Insta_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insta_cloneApp.swift
+//  Insta_clone
+//
+//  Created by 野中淳 on 2025/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Insta_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
